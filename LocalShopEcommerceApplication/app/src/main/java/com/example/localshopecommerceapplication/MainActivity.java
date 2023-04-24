f83fc4acce2c4379cc42f4c99c2fca08342f6f23
April Hussey <april.hussey@outlook.com>
2023-04-24 10:30:23 +0100

    Created fragments and made bottom navigation bar functional

--- LocalShopEcommerceApplication/app/src/main/java/com/example/localshopecommerceapplication/MainActivity.java
+++ LocalShopEcommerceApplication/app/src/main/java/com/example/localshopecommerceapplication/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.localshopecommerceapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    MenuItem navHome;
+    MenuItem navShop;
+    MenuItem navBasket;
+    MenuItem navWishlist;
+    MenuItem navAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navHome) {
+                    selectedFragment = new HomeFragment();
+                } else if (itemId == R.id.navShop) {
+                    selectedFragment = new ShopFragment();
+                } else if (itemId == R.id.navBasket) {
+                    selectedFragment = new BasketFragment();
+                } else if (itemId == R.id.navWishlist) {
+                    selectedFragment = new WishlistFragment();
+                } else if (itemId == R.id.navAccount) {
+                    selectedFragment = new AccountFragment();
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, selectedFragment).commit();
+                return true;
+            }
+        });
     }
 }
